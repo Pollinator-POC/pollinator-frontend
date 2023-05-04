f0eb206954258ae396d0f939488899eca3cdc7d4
Andreea Mandescu <andreea-valentina.mandescu@bp.com>
2023-05-04 12:01:18 +0100

    feat: retrieve chargepoints and create table

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import App from "./App";
-import { render, screen} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 
-describe('App.tsx',  () => {
-    it('should render the application',  async () => {
-        render(<App/>);
-        const helloWorldText = await screen.getByText("Hello World");
-        expect(helloWorldText).toBeTruthy();
+(global.fetch as jest.Mock) = jest.fn(() =>
+    Promise.resolve({
+        json: () => Promise.resolve([{}])
+    })
+);
+
+describe("App.tsx", () => {
+    it("should render the application", async () => {
+        render(<App />);
+
+        const chargingPointId = await screen.getByText("Charging Point Id");
+        const status = await screen.getByText("Status");
+
+        expect(chargingPointId).toBeTruthy();
+        expect(status).toBeTruthy();
     });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
-import React, { FunctionComponent } from "react";
+import React from "react";
+import {useState, useEffect} from "react";
 
-const App: FunctionComponent = () => <div>Hello World</div>;
+const styles: {[key: string]: React.CSSProperties} = {
+    table: {
+        borderCollapse: "collapse",
+        border: "1px solid",
+        width: "50%",
+        padding: "8px"
+    },
+    th: {
+        border: "1px solid",
+        padding: "8px"
+    }
+};
+
+const App = () => {
+    const [chargePoints, setChargePoints] = useState([]);
+    const getChargePoints = async () =>
+        await fetch("http://localhost:8080/charge_point_live_status")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setChargePoints(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+
+    useEffect(() => {
+        const interval = setInterval(async () => getChargePoints(), 10);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <table style={styles.table}>
+            <thead>
+                <tr>
+                    <th style={styles.th}>Charging Point Id</th>
+                    <th style={styles.th}>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {chargePoints.map((cp) => (
+                    <tr key={cp.charge_point_id}>
+                        <th style={styles.th}>{cp.charge_point_id}</th>
+                        <th style={styles.th}>{cp.status}</th>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
 
 export default App;
